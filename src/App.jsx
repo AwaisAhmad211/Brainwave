5806f0e277c00fac48c94594a2c865021e6141c3
Awais Ahmad <itxawais211@gmail.com>
2024-09-07 08:38:18 -0700

    Services Section Completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Button from "./components/Button"
 import Collaboration from "./components/Collaboration"
 import Header from "./components/Header"
 import Hero from "./components/Hero"
+import Services from "./components/Services"
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
       <Hero />
       <Benefits />
       <Collaboration />
+      <Services />
      </div>
     </>
   )
